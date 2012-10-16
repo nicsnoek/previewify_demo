9c1bde3943e74a3355499fc99487849038e2ee86
Nic Snoek <nicsnoek@yahoo.com>
2012-10-16 14:58:07 +1100

    Appropriate comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery
+
+  # Previewify::Control allows you to switch between preview and live mode
   include Previewify::Control
 
 end
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,12 +2,9 @@ class HomepageController < ApplicationController
 
   before_filter :preview_or_published
 
-  def maybe_some_function_of_current_user_or_hostname?
-    params.has_key?(:preview) # But in this case just a simple parameter
-  end
-
   def preview_or_published
-      show_preview(maybe_some_function_of_current_user_or_hostname?)
+    #This could be a function of current user, hostname, a parameter etc.
+    show_preview(params.has_key?(:preview))
   end
 
 
